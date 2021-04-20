68a99162c5a510e44ebac7d3ac2139f71e48da00
prabodhw96 <prabodhwankhede1996@gmail.com>
2021-04-20 18:44:11 +0530

    Add late concatenation

--- app.py
+++ app.py
@@ -43,13 +43,13 @@ def plot_loss(filename, title):
     fig.add_trace(go.Scatter(x=epoch, y=valid_loss, mode="lines+markers", name="valid loss"))
     fig.add_trace(go.Scatter(x=[loaded_epoch], y=[valid_loss[loaded_epoch-1]], mode="markers", name="Epoch loaded", 
     			  marker=dict(color="#f8766d", size=8), showlegend=True, hoverinfo="skip"))
-    fig.update_layout(title="{} {}<br>Test PER: {}".format(title, env, test_per), yaxis_range=[0,3.5],
+    fig.update_layout(title="{} {}<br>Test PER: {}".format(title, env, test_per), yaxis_range=[0,10] if "late" in filename else [0,3.5],
                       title_x=0.5, xaxis_title="Epoch", yaxis_title="Loss", hovermode="x unified")
     return fig
 
 
 title_list = ["Delay and sum beamforming", "MVDR beamforming", "GeV beamforming", "Single channel",
-			  "Early concatenation", "BeamformIt", "Beamforming on the fly"]
+			  "Early concatenation", "BeamformIt", "Beamforming on the fly", "Late concatenation"]
 #title = st.selectbox("Select Experiment", title_list)
 title = st.radio("Select Experiment", tuple(title_list))
 
@@ -61,6 +61,7 @@ title_file_dict["Single channel"] = "train_log_la2"
 title_file_dict["Early concatenation"] = "train_log_early"
 title_file_dict["BeamformIt"] = "train_log_beamformit"
 title_file_dict["Beamforming on the fly"] = "train_log_fly"
+title_file_dict["Late concatenation"] = "train_log_late"
 
 filename = title_file_dict[title] + ".txt"
 filename_env = title_file_dict[title] + "_env.txt"
